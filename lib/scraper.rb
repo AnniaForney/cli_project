411a843c2cd62a812400dc8a324dd05a8e1a11d3
Annia Forney <anniaforney@gmail.com>
2019-07-14 20:51:36 +0000

    cli

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,11 @@
 class Scraper 
+  URL = "https://www.feelcrystals.com.au/crystal-meanings/"
+   doc = Nokogiri::HTML(html)
+   
+  def self.scrape_crystal_name
+  end 
   
+  def self.scrape_crystal 
+  end 
   
 end 
