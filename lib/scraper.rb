f2fda2cb8a7bd4ecc58ee2fbf1bf6193b817c50d
Annia Forney <anniaforney@gmail.com>
2019-07-26 16:41:48 +0000

    scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,16 +8,16 @@ class Scraper
   
   def self.scrape_meanings
     page = self.open_page
-    crystals = page.css(".product-category")
+    crystals = page.css(".formcontainC")
     crystals.each do |crys| 
-    name = crys.css(".collection-wrapper h3").text.strip
+    name = crys.css(".left").text.strip
     crystal = Crystal.new(name) 
-    crystal.meaning = crys.css("h1").text.strip 
+    crystal.meaning = crys.css("center").text.strip 
     end 
   end 
   
   def self.crystal_names
-    crystal_names = crystal_names.css(".product-category").css(".term-description").css(".collection-wrapper").map {|crystal| crystal_names}
+    crystal_names = crystal_names.css(".formcontainC").css(".left").css(".center").map {|crystal| crystal_names}
   end 
   
   
